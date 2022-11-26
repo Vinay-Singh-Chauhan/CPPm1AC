12926bbba87babee7fae5339754615b904783928
Vinay Singh Chauhan <chauhansvinay@gmail.com>
2022-11-26 14:12:49 +0530

    DSU_START

---
+++ Disjoint_Set_Union/dsu.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int N=1e5+2;
+vector<int> parent(N);
+vector<int> size(N);
+void make_set(){
+    
+}
+int main()
+{
+
+    return 0;   
+}
